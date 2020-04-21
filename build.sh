dee5bc21c7929cf1d9179afbd8b1b931eae6ffc0
Elena Martinez <emartinez@cloudbees.net>
2020-04-21 15:02:16 -0400

    Create a target directory and some jars in the build script.

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "Hello this is start of the build script. " $(date)
+mkdir target
+cd ./target
+touch my.jar
+touch your.jar
 echo "Goodbye this is end of the build script. " $(date)
