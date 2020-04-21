4c8b144c202193fa1b25325f557e4c42ff660416
Elena Martinez <emartinez@cloudbees.net>
2020-04-21 14:50:05 -0400

    Add shell scripts.

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Hello this is start of the build script. " $(date)
+echo "Goodbye this is end of the build script. " $(date)
---
+++ test-all.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Hello this is start of the test script. " $(date)
+echo "Goodbye this is end of the test script. " $(date)
